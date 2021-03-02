81ee5eea6cfd89186e9ea05abfe91f80a6bcb208
priyam23 <jhijhipoka23@gmail.com>
2021-03-02 05:51:04 -0500

    changes in header files

--- test1.cpp
+++ test1.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
-using namespace std;
-
-char *buffer = NULL;
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
 
 int main(int argc, char **argv)
 {
     int size1 = 5;
     int size2 = size1 + 5;
-    char *dst = malloc(size1); // allocation size is 5 bytes
-    char *src = malloc(size2); // allocation size is 10 bytes
-    if (strcmp(argv, "change") == 0)
+    char *dst = (char*)malloc(size1); // allocation size is 5 bytes
+    char *src = (char*)malloc(size2); // allocation size is 10 bytes
+    if (strcmp(argv[1], "change") == 0)
         size1 = 15;
         
     memcpy(dst, src, size1);
+    
+    return 0;
 }
-
 
